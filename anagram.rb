350d6b495d4a15147df084b750f37ffa6820f36f
Igors78 <oleinikovsigors@gmail.com>
2020-11-21 22:17:07 +0200

    rubocop errors fixed

--- anagram.rb
+++ anagram.rb
@@ -1,18 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def anagram(str)
-  return -1 if str.length.odd?
-
-  str1 = str.split('').take(str.length / 2).sort
-  str2 = str.split('').slice(-str.length / 2, str.length - 1).sort
-  return 0 if str1 == str2
-
-  (0...str1.length).each do |i|
-    str2.delete_at(str2.index(str1[i])) if str2.index(str1[i])
-  end
-  str2.length
-end
-
 str = 'fdhlvosfpafhalll'
-p anagram(str)
+puts(-1) if str.length.odd?
+
+str1 = str.split('').take(str.length / 2).sort
+str2 = str.split('').slice(-str.length / 2, str.length - 1).sort
+puts 0 if str1 == str2
+
+(0...str1.length).each do |i|
+  str2.delete_at(str2.index(str1[i])) if str2.index(str1[i])
+end
+puts str2.length
