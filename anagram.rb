552193099c147d8351a784899452e71d86f566fa
Igors78 <oleinikovsigors@gmail.com>
2020-11-22 09:10:42 +0200

    Meteoservice added

--- anagram.rb
+++ anagram.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-str = 'fdhlvosfpafhalll'
-puts(-1) if str.length.odd?
+str = 'vallaver'
+abort '-1' if str.length.odd?
 
 str1 = str.split('').take(str.length / 2).sort
 str2 = str.split('').slice(-str.length / 2, str.length - 1).sort
-puts 0 if str1 == str2
+abort '0' if str1 == str2
 
 (0...str1.length).each do |i|
   str2.delete_at(str2.index(str1[i])) if str2.index(str1[i])
